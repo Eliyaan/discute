9a4479cc45ebd4a5f7c108bb6af7b40e9311b7a3
Nopana_Eliyaan <nopana1007@gmail.com>
2026-05-05 12:20:53 +0200

    )

--- discute.c
+++ discute.c
@@ -30,14 +30,58 @@ typedef struct {
 	unsigned int group;
 	unsigned int channel;
 	unsigned int bottom_message; /* reference message for height, the one at the bottom of the screen */
-	unsigned int bottom_message_y; /* 
+	unsigned int bottom_message_y; /* y coord of the bottom of the bottom message */
 } Client;
 
 void frame(Client client) {
+	int i, dist_start, message_y;
+	Channel* cur_channel;
+	ChannelGroup cur_group;
+	Server cur_server;
+	unsigned int cap, start, length;
+	unsigned int* heights;
+
 	/* Update the state according to inputs*/
 	client.y += 10; /* scroll */
+
+	/* Update the backend: fetch new messages... */
+
+
 	
+	cur_server = client.servers[client.server];
+	cur_group = cur_server.groups[client.group];
+	cur_channel= &cur_group.channels[client.channel];
+
 	/* Find all the messages that need to be drawn, and query new ones if needed */
+	/* Find the message at the bottom of the screen if it changed */
+	heights = cur_channel->heights;
+	cap = cur_channel->cap;
+	start = cur_channel->start;
+	length = cur_channel->length;
+
+	if (length > 0) 
+	{
+		message_y = client.bottom_message_y;
+		i = client.bottom_message;
+		dist_start = i - start + 1;
+		message_y += heights[i]; 
+		while(message_y < client.y && dist_start < length)
+		{
+			i++;
+			dist_start++;
+			if (i >= cap)
+			{
+				i = 0;
+			}
+			message_y += heights[i];
+			
+		}
+		client.bottom_message = i;
+		client.bottom_message_y = message_y - heights[i];
+	}
 	
+	/* Find the last message (at the top of the screen) */
+	/* Query more messages if we need more */
+	/* Update the circular buffer, set the old messages to freed */
 	
 }
